1760e2b9e8e4ee7dcf29eea1470d2f2a2f0b8b8d
David Chou <davidchou2009@gmail.com>
2022-01-26 09:25:07 -0800

    When I enter in engineer or intern info, I am greeted with a menu to decide if i want to add another employee

--- index.js
+++ index.js
@@ -15,7 +15,70 @@ function writeToFile(fileName, data) {
         err ? console.error(err) : console.log('Success!')
     );
 }
+const addEmployee = () => {
+    console.log(`===========
+Adding Employee
+===========`)
+    inquirer
+        .prompt([
+            {
+                type: 'list',
+                message: "Select employee role: ",
+                name: 'role',
+                choices: ['Engineer', 'Intern'],
+            },
+            {
+                type: 'name',
+                message: "Employee's name: ",
+                name: 'name',
+            },
+            {
+                type: 'input',
+                message: "Employee's ID: ",
+                name: 'id',
+            },
+            {
+                type: 'input',
+                message: "Employee's email: ",
+                name: 'email',
+            },
+            {
+                type: 'input',
+                message: "Employee's github username: ",
+                name: 'github',
+                when: (input) => input.role === "Engineer",
+            },
+            {
+                type: 'input',
+                message: "Intern's school: ",
+                name: 'school',
+                when: (input) => input.role === "Intern",
+            },
+            {
+                type: 'confirm',
+                message: "Add another employee? ",
+                name: 'addEmployee',
+                default: false,
+            },
 
+        ])
+        .then((employeeResponse) => {
+            let employee;
+            console.log('role: ',employeeResponse.role);
+            if (employeeResponse.role === "Engineer") {
+                employee = new Engineer(employeeResponse.name, employeeResponse.id, employeeResponse.email, employeeResponse.github);
+            } else if (employeeResponse.role === "Intern") {
+                employee = new Intern(employeeResponse.name, employeeResponse.id, employeeResponse.email, employeeResponse.school);
+            }
+            teamCards.push(employee);
+            if (employeeResponse.addEmployee) {
+                return addEmployee();
+            } else {
+                return teamCards;
+            }
+
+        });
+}
 // TODO: Create a function to initialize app
 function init() {
     inquirer
@@ -45,125 +108,11 @@ function init() {
         .then((response) => {
             const manager = new Manager(response.name, response.id, response.email, response.number);
             teamCards.push(manager);
+            addEmployee();
 
-            inquirer
-            .prompt([
-                {
-                    type: 'list',
-                    message: "Select employee role: ",
-                    name: 'role',
-                    choices: ['Engineer', 'Intern'],
-                },
-                {
-                    type: 'name',
-                    message: "Employee's name: ",
-                    name: 'id',
-                },
-                {
-                    type: 'input',
-                    message: "Employee's ID: ",
-                    name: 'id',
-                },
-                {
-                    type: 'input',
-                    message: "Employee's email: ",
-                    name: 'email',
-                },
-                {
-                    type: 'input',
-                    message: "Employee's github username: ",
-                    name: 'github',
-                    when: (input) => input.role === "Engineer",
-                },
-                {
-                    type: 'input',
-                    message: "Intern's school: ",
-                    name: 'github',
-                    when: (input) => input.role === "Intern",
-                },
-                {
-                    type: 'list',
-                    message: "Add another employee? ",
-                    name: 'addEmployee',
-                    choices: ['yes', 'no']
-                },
-
-            ])
-            .then((employeeResponse) => {
-                let employee;
-
-                if (employeeResponse.role === "Engineer") {
-                    employee = new Engineer(employeeResponse.name, employeeResponse.id, employeeResponse.email, employeeResponse.github);
-                } else if (response.role === "Intern") {
-                    employee = new Intern(employeeResponse.name, employeeResponse.id, employeeResponse.email, employeeResponse.school);
-                }
-                teamCards.push(employee);
-                if(employeeResponse.addEmployee == "yes"){
-                    return addEmployee();
-                }else{
-                    return teamCards;
-                }
-
-            });
         });
-    
-    // const addEmployee = () => {
-    //     inquirer
-    //         .prompt([
-    //             {
-    //                 type: 'list',
-    //                 message: "Select employee role: ",
-    //                 name: 'role',
-    //                 choices: ['Engineer', 'Intern'],
-    //             },
-    //             {
-    //                 type: 'input',
-    //                 message: "Employee's ID: ",
-    //                 name: 'id',
-    //             },
-    //             {
-    //                 type: 'input',
-    //                 message: "Employee's email: ",
-    //                 name: 'email',
-    //             },
-    //             {
-    //                 type: 'input',
-    //                 message: "Employee's github username: ",
-    //                 name: 'github',
-    //                 when: (input) => input.role === "Engineer",
-    //             },
-    //             {
-    //                 type: 'input',
-    //                 message: "Intern's school: ",
-    //                 name: 'github',
-    //                 when: (input) => input.role === "Intern",
-    //             },
-    //             {
-    //                 type: 'list',
-    //                 message: "Add another employee? ",
-    //                 name: 'addEmployee',
-    //                 choices: ['yes', 'no']
-    //             },
 
-    //         ])
-    //         .then((response) => {
-    //             let employee;
 
-    //             if (response.role === "Engineer") {
-    //                 employee = new Engineer(response.name, response.id, response.email, response.github);
-    //             } else if (response.role === "Intern") {
-    //                 employee = new Intern(response.name, response.id, response.email, response.school);
-    //             }
-    //             teamCards.push(employee);
-    //             if(response.addEmployee == "yes"){
-    //                 return addEmployee();
-    //             }else{
-    //                 return teamCards;
-    //             }
-
-    //         });
-    // }
-    // addManager().then(addEmployee);
 }
 // Function call to initialize app
 init();
